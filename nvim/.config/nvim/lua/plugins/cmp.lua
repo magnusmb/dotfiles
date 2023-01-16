c69f48c637088d52ed64af17d215ff9b9301006e
Magnus Myhrvold Bratteng <magnus.bratteng@knowit.no>
2023-01-16 12:46:23 +0100

    Add Emmet completion

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -10,6 +10,10 @@ return {
 		-- Snippets
 		"L3MON4D3/LuaSnip",
 		"rafamadriz/friendly-snippets",
+
+		-- Emmet
+		"dcampos/cmp-emmet-vim",
+		"mattn/emmet-vim",
 	},
 	config = function()
 		vim.opt.completeopt = "menu,menuone,noselect"
@@ -33,19 +37,21 @@ return {
 				["<C-y>"] = cmp.config.disable,
 				["<Tab>"] = cmp.mapping.confirm({ select = true }),
 			}),
-			-- formatting = {
-			-- 	format = function(entry, vim_item)
-			-- 		vim_item.menu = ({
-			-- 			buffer = "[Buffer]",
-			-- 			nvim_lsp = "[Lsp]",
-			-- 			luasnip = "[Snippet]",
-			-- 			path = "[Path]",
-			-- 		})[entry.source.name]
-			-- 		return vim_item
-			-- 	end,
-			-- },
+			formatting = {
+				format = function(entry, vim_item)
+					vim_item.menu = ({
+						emmet_vim = "[Emmet]",
+						buffer = "[Buffer]",
+						nvim_lsp = "[Lsp]",
+						luasnip = "[LuaSnip]",
+						path = "[Path]",
+					})[entry.source.name]
+					return vim_item
+				end,
+			},
 			view = { entries = { name = "custom", selection_order = "near_cursor" } },
 			sources = {
+				{ name = "emmet_vim" },
 				{ name = "luasnip" },
 				{ name = "nvim_lsp" },
 				{ name = "buffer" },
