135ded9c70cc63d6d80483471c15b698e477293b
Magnus Myhrvold Bratteng <magnus.bratteng@knowit.no>
2023-06-27 14:17:53 +0200

    Add yank highlight

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -58,3 +58,17 @@ require("lazy").setup("plugins", {
 	},
 })
 require("magnusmb.remaps")
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local yank_group = augroup("HighlightYank", {})
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
